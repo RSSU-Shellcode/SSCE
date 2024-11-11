6f96c0e455e1149b4e4cc1452970c91fe6c259b0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-11 22:11:31 +0800

    add a script for clean files that generated by Visual Studio.

---
+++ decoder/clean.bat
@@ -0,0 +1,8 @@
+rd  /S /Q ".vs\decoder\v17\ipch"
+del /S /Q ".vs\decoder\v17\Browse.VC.db"
+del /S /Q ".vs\decoder\v17\Solution.VC.db"
+
+rd /S /Q "Debug"
+rd /S /Q "Release"
+rd /S /Q "x64"
+rd /S /Q "x86"
