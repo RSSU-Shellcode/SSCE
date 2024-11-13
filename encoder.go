b1d1352b0aa5d9396946cc809d25f9faed3e9979
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-13 21:02:45 +0800

    replace experimental code about build shellcode to go template.

--- encoder.go
+++ encoder.go
@@ -1,10 +1,11 @@
 package ssce
 
 import (
-	"encoding/binary"
+	"bytes"
 	"errors"
 	"fmt"
 	"math/rand"
+	"text/template"
 	"time"
 
 	"github.com/For-ACGN/go-keystone"
@@ -50,56 +51,43 @@ func NewEncoder(arch int) (*Encoder, error) {
 
 // Encode is used to encode input shellcode to a unique shellcode.
 func (e *Encoder) Encode(shellcode []byte) ([]byte, error) {
-	key := e.randBytes(32)
-	cipher := encrypt(shellcode, key)
-
-	output := make([]byte, 0, 1024+len(shellcode))
-	output = append(output, e.genGarbageJumpShort(64)...)
-	output = append(output, e.saveContext()...)
+	tpl, err := template.New("asm_src").Funcs(template.FuncMap{
+		"db":  toDB,
+		"hex": toHex,
+		"igi": func() string {
+			return ";" + toDB(e.genGarbageInst())
+		},
+	}).Parse(x64asm)
+	if err != nil {
+		return nil, fmt.Errorf("invalid assembly source template: %s", err)
+	}
+	cryptoKey := e.randBytes(32)
+	shellcode = encrypt(shellcode, cryptoKey)
+	jump := e.genGarbageJumpShort(64)
+	save := e.saveContext()
 	restore := e.restoreContext()
+	ctx := asmContext{
+		JumpShort:      jump,
+		SaveContext:    save,
+		RestoreContext: restore,
+		DecryptorStub:  x64Decoder,
+		CleanerStub:    nil,
+		CryptoKey:      cryptoKey,
+		CryptoKeyLen:   len(cryptoKey),
+		Shellcode:      shellcode,
+		ShellcodeLen:   len(shellcode),
+	}
+	buf := bytes.NewBuffer(make([]byte, 0, 2048+5*len(shellcode)))
+	err = tpl.Execute(buf, &ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build assembly source: %s", err)
+	}
 
-	call0 := make([]byte, 1+4)
-	call0[0] = 0xE8
-	binary.LittleEndian.PutUint32(call0[1:], uint32(5+len(restore)+1))
-
-	call1 := make([]byte, 1+4)
-	call1[0] = 0xE8
-	binary.LittleEndian.PutUint32(call1[1:], uint32(len(restore)+1+len(x64Decoder)))
-
-	src := `
-.code64
-
-entry:
-  call get_rip
-  jmp next
-get_rip:
-  pop rbx
-  push rbx
-  ret
-next:
-  lea rcx, [rbx + EOF - 5 + 0x%X]
-  mov rdx, 0x%X
-  lea r8, [rbx + EOF - 5 + 0x%X]
-  mov r9, 0x%X
-EOF:
-`
-	cipherPtr := len(call0) + len(call1) + len(restore) + 1 + len(x64Decoder)
-	keyPtr := len(call0) + len(call1) + len(restore) + 1 + len(x64Decoder) + len(cipher)
-	src = fmt.Sprintf(src, cipherPtr, len(cipher), keyPtr, len(key))
-
-	inst, err := e.engine.Assemble(src, 0)
+	inst, err := e.engine.Assemble(buf.String(), 0)
 	if err != nil {
 		return nil, err
 	}
-	output = append(output, inst...)
-	output = append(output, call0...)
-	output = append(output, call1...)
-	output = append(output, restore...)
-	output = append(output, 0xC3)
-	output = append(output, x64Decoder...)
-	output = append(output, cipher...)
-	output = append(output, key...)
-	return output, nil
+	return inst, nil
 }
 
 // Close is used to close shellcode encoder.
