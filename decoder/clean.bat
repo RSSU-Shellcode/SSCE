db4bfdc8c42df89fcaf8292dd412d5fae90c9051
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-13 20:53:55 +0800

    clean.bat: add rd /S /Q decoder for delete directory that generated by compiler.

--- decoder/clean.bat
+++ decoder/clean.bat
@@ -2,6 +2,8 @@ rd  /S /Q ".vs\decoder\v17\ipch"
 del /S /Q ".vs\decoder\v17\Browse.VC.db"
 del /S /Q ".vs\decoder\v17\Solution.VC.db"
 
+rd /S /Q "decoder"
+
 rd /S /Q "Debug"
 rd /S /Q "Release"
 rd /S /Q "x64"
