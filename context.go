645fb423ffba134be9e322807ef2f4d385241041
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-15 21:05:54 +0800

    update x64SaveContext and x64RestoreContext about pushfq and popfq.

--- context.go
+++ context.go
@@ -5,26 +5,29 @@ var (
 	x86RestoreContext = [][]byte{{0x61}, {0x9D}} // popad, popfd
 
 	x64SaveContext = [][]byte{
-		{0x50}, {0x53}, {0x51}, {0x52}, // push rax, rbx, rcx, rdx
-		{0x56}, {0x57}, {0x55}, {0x54}, // push rsi, rdi, rbp, rsp
-		{0x41, 0x50}, {0x41, 0x51}, // push r8, r9
-		{0x41, 0x52}, {0x41, 0x53}, // push r10, r11
-		{0x41, 0x54}, {0x41, 0x55}, // push r12, r13
-		{0x41, 0x56}, {0x41, 0x57}, // push r14, r15
+		{0x50}, {0x53}, {0x51}, {0x52}, //    push rax, rbx, rcx, rdx
+		{0x56}, {0x57}, {0x55}, {0x54}, //    push rsi, rdi, rbp, rsp
+		{0x41, 0x50}, {0x41, 0x51}, //        push r8, r9
+		{0x41, 0x52}, {0x41, 0x53}, //        push r10, r11
+		{0x41, 0x54}, {0x41, 0x55}, //        push r12, r13
+		{0x41, 0x56}, {0x41, 0x57}, //        push r14, r15
+		{0x9C}, //                            pushfq
+
 	}
 	x64RestoreContext = [][]byte{
-		{0x58}, {0x5B}, {0x59}, {0x5A}, // pop rax, rbx, rcx, rdx
-		{0x5E}, {0x5F}, {0x5D}, {0x5C}, // pop rsi, rdi, rbp, rsp
-		{0x41, 0x58}, {0x41, 0x59}, // pop r8, r9
-		{0x41, 0x5A}, {0x41, 0x5B}, // pop r10, r11
-		{0x41, 0x5C}, {0x41, 0x5D}, // pop r12, r13
-		{0x41, 0x5E}, {0x41, 0x5F}, // pop r14, r15
+		{0x58}, {0x5B}, {0x59}, {0x5A}, //    pop rax, rbx, rcx, rdx
+		{0x5E}, {0x5F}, {0x5D}, {0x5C}, //    pop rsi, rdi, rbp, rsp
+		{0x41, 0x58}, {0x41, 0x59}, //        pop r8, r9
+		{0x41, 0x5A}, {0x41, 0x5B}, //        pop r10, r11
+		{0x41, 0x5C}, {0x41, 0x5D}, //        pop r12, r13
+		{0x41, 0x5E}, {0x41, 0x5F}, //        pop r14, r15
+		{0x9D}, //                            popfq
 	}
 )
 
-func (e *Encoder) saveContext(arch int) []byte {
+func (e *Encoder) saveContext() []byte {
 	var save [][]byte
-	switch arch {
+	switch e.arch {
 	case 32:
 		save = x86SaveContext
 	case 64:
@@ -39,9 +42,9 @@ func (e *Encoder) saveContext(arch int) []byte {
 	return inst
 }
 
-func (e *Encoder) restoreContext(arch int) []byte {
+func (e *Encoder) restoreContext() []byte {
 	var restore [][]byte
-	switch arch {
+	switch e.arch {
 	case 32:
 		restore = x86RestoreContext
 	case 64:
