7ddbf97b84a307a6b7fd8b822447669ec4755b0e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-15 21:26:41 +0800

    add Options for encode shellcode, improve obfuscation about padding garbage.

--- encoder.go
+++ encoder.go
@@ -17,16 +17,25 @@ type Encoder struct {
 	engine64 *keystone.Engine
 	rand     *rand.Rand
 
+	// context arguments
+	arch int
+	opts *Options
+
+	// save and restore context
 	contextSeq []int
 }
 
 // Options contains options about Encode.
 type Options struct {
-	SaveContext bool // not break registers
+	NumIter     int
+	NumTailInst int
+	SaveContext bool
+	NoIterator  bool
+	NoGarbage   bool
 }
 
-// NewEncoder is used to create a simple shellcode encoder.
-func NewEncoder() (*Encoder, error) {
+// New is used to create a simple shellcode encoder.
+func New() (*Encoder, error) {
 	var ok bool
 	engine32, err := keystone.NewEngine(keystone.ARCH_X86, keystone.MODE_32)
 	if err != nil {
@@ -81,9 +90,15 @@ func (e *Encoder) Encode(shellcode []byte, arch int, opts *Options) ([]byte, err
 		return nil, err
 	}
 	// iterate the encoding of the pre-decoder and part of the shellcode
+	numIter := opts.NumIter
+	if numIter < 1 {
+		numIter = 4 + e.rand.Intn(8)
+	}
+	if opts.NoIterator {
+		numIter = 0
+	}
 	divider := e.rand.Intn(len(output))
-	times := 4 + e.rand.Intn(8)
-	for i := 0; i < times; i++ {
+	for i := 0; i < numIter; i++ {
 		input := output[:divider]
 		newOutput, err := e.encode(input, arch, opts)
 		if err != nil {
@@ -93,7 +108,10 @@ func (e *Encoder) Encode(shellcode []byte, arch int, opts *Options) ([]byte, err
 		divider = e.rand.Intn(len(newOutput))
 	}
 	// padding garbage at the tail
-	times = 8 + e.rand.Intn(32)
+	times := 8 + e.rand.Intn((numIter+1)*16)
+	if opts.NoGarbage {
+		times = 0
+	}
 	for j := 0; j < times; j++ {
 		var garbage []byte
 		switch e.rand.Intn(3) {
@@ -104,31 +122,40 @@ func (e *Encoder) Encode(shellcode []byte, arch int, opts *Options) ([]byte, err
 		}
 		output = append(output, garbage...)
 	}
+	// append garbage data to tail for prevent brute-force
+	output = append(output, e.randBytes(opts.NumTailInst)...)
 	return output, nil
 }
 
 func (e *Encoder) encode(shellcode []byte, arch int, opts *Options) ([]byte, error) {
-	var engine *keystone.Engine
+	var (
+		engine *keystone.Engine
+		asmTpl string
+	)
 	switch arch {
 	case 32:
 		engine = e.engine32
+		asmTpl = ""
 	case 64:
 		engine = e.engine64
+		asmTpl = x64asm
 	default:
 		return nil, errors.New("invalid architecture")
 	}
+	e.arch = arch
+	e.opts = opts
 	tpl, err := template.New("asm_src").Funcs(template.FuncMap{
 		"db":  toDB,
 		"hex": toHex,
 		"igi": func() string {
 			return ";" + toDB(e.garbageInst())
 		},
-	}).Parse(x64asm)
+	}).Parse(asmTpl)
 	if err != nil {
 		return nil, fmt.Errorf("invalid assembly source template: %s", err)
 	}
 	cryptoKey := e.randBytes(32)
-	shellcode = encrypt(shellcode, cryptoKey)
+	shellcode = encrypt32(shellcode, cryptoKey)
 	jump := e.garbageJumpShort(64)
 	ctx := asmContext{
 		JumpShort:      jump,
@@ -142,8 +169,8 @@ func (e *Encoder) encode(shellcode []byte, arch int, opts *Options) ([]byte, err
 		ShellcodeLen:   len(shellcode),
 	}
 	if opts.SaveContext {
-		ctx.SaveContext = e.saveContext(arch)
-		ctx.RestoreContext = e.restoreContext(arch)
+		ctx.SaveContext = e.saveContext()
+		ctx.RestoreContext = e.restoreContext()
 	}
 	buf := bytes.NewBuffer(make([]byte, 0, 2048+5*len(shellcode)))
 	err = tpl.Execute(buf, &ctx)
