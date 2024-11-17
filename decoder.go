77f9711b3eabd7d6a4264796a301d947f446318a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-17 21:35:04 +0800

    add methods about create instruction builder, finish instBuilder64 and selectOpSize64.

--- decoder.go
+++ decoder.go
@@ -2,6 +2,8 @@ package ssce
 
 import (
 	"encoding/binary"
+	"fmt"
+	"strings"
 )
 
 func (e *Encoder) decoderBuilder() []byte {
@@ -40,6 +42,95 @@ func (e *Encoder) shellcodeBuilder() []byte {
 	builder := make([]byte, 0, 512)
 	builder = append(builder, e.sc...)
 	return builder
+}
+
+// lea reg, [rbx + offset + index]
+// mov word ptr [reg], 0x1234
+func (e *Encoder) instBuilder(inst []byte, offset string) string {
+	switch e.arch {
+	case 32:
+		return e.instBuilder32(inst, offset)
+	case 64:
+		return e.instBuilder64(inst, offset)
+	default:
+		panic("invalid architecture")
+	}
+}
+
+func (e *Encoder) instBuilder32(inst []byte, offset string) string {
+	builder := strings.Builder{}
+	return builder.String()
+}
+
+func (e *Encoder) selectOpSize32(rem int) int {
+	var size []int
+	switch {
+	case rem < 2:
+		size = []int{1}
+	case rem < 4:
+		size = []int{1, 2}
+	default:
+		size = []int{1, 2, 4}
+	}
+	return size[e.rand.Intn(len(size))]
+}
+
+func (e *Encoder) instBuilder64(inst []byte, offset string) string {
+	builder := &strings.Builder{}
+	registers := []string{
+		"rax", "rcx", "rdx", "rsi", "rdi",
+		"r8", "r9", "r10", "r11",
+		"r12", "r13", "r14", "r15",
+	}
+	rem := len(inst)
+	var index int
+	for {
+		if rem == 0 {
+			break
+		}
+		reg := registers[e.rand.Intn(len(registers))]
+		opSize := e.selectOpSize64(rem)
+		var (
+			op string
+			im int
+		)
+		switch opSize {
+		case 1:
+			op = "byte"
+			im = int(inst[index])
+		case 2:
+			op = "word"
+			im = int(binary.LittleEndian.Uint16(inst[index:]))
+		case 4:
+			op = "dword"
+			im = int(binary.LittleEndian.Uint32(inst[index:]))
+		case 8:
+			op = "qword"
+			im = int(binary.LittleEndian.Uint64(inst[index:]))
+		}
+		asm := "lea %s, [rbx + %s + 0x%X] {{igi}}\n"
+		_, _ = fmt.Fprintf(builder, asm, reg, offset, index)
+		asm = "mov %s ptr [%s], 0x%X {{igi}}\n"
+		_, _ = fmt.Fprintf(builder, asm, op, reg, im)
+		rem -= opSize
+		index += opSize
+	}
+	return builder.String()
+}
+
+func (e *Encoder) selectOpSize64(rem int) int {
+	var size []int
+	switch {
+	case rem < 2:
+		size = []int{1}
+	case rem < 4:
+		size = []int{1, 2}
+	case rem < 8:
+		size = []int{1, 2, 4}
+	default:
+		size = []int{1, 2, 4, 8}
+	}
+	return size[e.rand.Intn(len(size))]
 }
 
 func (e *Encoder) decoderStub() []byte {
