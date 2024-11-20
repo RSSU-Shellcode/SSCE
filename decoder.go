81cf89c90ce6608c4de502a3c7922f7d34b884e5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-20 20:36:41 +0800

    improve Encoder.miniXOR64 that add xorShift64, remove methods about create builder.

--- decoder.go
+++ decoder.go
@@ -3,145 +3,7 @@ package ssce
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
-	"strings"
 )
-
-func (e *Encoder) decoderBuilder() string {
-	var decoder []byte
-	switch e.arch {
-	case 32:
-		decoder = x86Decoder
-	case 64:
-		decoder = x64Decoder
-	}
-	return e.instBuilder(decoder, "decoder_stub")
-}
-
-func (e *Encoder) eraserBuilder() string {
-	var eraser []byte
-	switch e.arch {
-	case 32:
-		eraser = x86Eraser
-	case 64:
-		eraser = x64Eraser
-	}
-	return e.instBuilder(eraser, "eraser_stub")
-}
-
-func (e *Encoder) cryptoKeyBuilder() string {
-	return e.instBuilder(e.key, "crypto_key_stub")
-}
-
-// lea reg, [rbx + offset + index]
-// mov word ptr [reg], 0x1234
-func (e *Encoder) instBuilder(inst []byte, offset string) string {
-	switch e.arch {
-	case 32:
-		return e.instBuilder32(inst, offset)
-	case 64:
-		return e.instBuilder64(inst, offset)
-	default:
-		panic("invalid architecture")
-	}
-}
-
-func (e *Encoder) instBuilder32(inst []byte, offset string) string {
-	builder := strings.Builder{}
-	return builder.String()
-}
-
-func (e *Encoder) selectOpSize32(rem int) int {
-	var size []int
-	switch {
-	case rem < 2:
-		size = []int{1}
-	case rem < 4:
-		size = []int{1, 2}
-	default:
-		size = []int{1, 2, 4}
-	}
-	return size[e.rand.Intn(len(size))]
-}
-
-func (e *Encoder) instBuilder64(inst []byte, offset string) string {
-	builder := &strings.Builder{}
-	registers := []string{
-		"rax", "rcx", "rdx", "rsi", "rdi",
-		"r8", "r9", "r10", "r11",
-		"r12", "r13", "r14", "r15",
-	}
-	rem := len(inst)
-	var index int
-	for {
-		if rem == 0 {
-			break
-		}
-		opSize := e.selectOpSize64(rem)
-		var (
-			op string
-			im uint
-		)
-		switch opSize {
-		case 1:
-			op = "byte"
-			im = uint(inst[index])
-		case 2:
-			op = "word"
-			im = uint(binary.LittleEndian.Uint16(inst[index:]))
-		case 4:
-			op = "dword"
-			im = uint(binary.LittleEndian.Uint32(inst[index:]))
-		case 8:
-			im = uint(binary.LittleEndian.Uint64(inst[index:]))
-		}
-		addr := registers[e.rand.Intn(len(registers))]
-		if opSize == 8 {
-			var val string
-			for {
-				val = registers[e.rand.Intn(len(registers))]
-				if val != addr {
-					break
-				}
-			}
-			asm := "  lea %s, [rbx + %s + 0x%X]"
-			asm += e.insertGarbageInst() + "\n"
-			_, _ = fmt.Fprintf(builder, asm, addr, offset, index)
-			asm = "  mov %s, 0x%X"
-			asm += e.insertGarbageInst() + "\n"
-			_, _ = fmt.Fprintf(builder, asm, val, im)
-			asm = "  mov [%s], %s"
-			asm += e.insertGarbageInst() + "\n"
-			_, _ = fmt.Fprintf(builder, asm, addr, val)
-		} else {
-			asm := "  lea %s, [rbx + %s + 0x%X]"
-			asm += e.insertGarbageInst() + "\n"
-			_, _ = fmt.Fprintf(builder, asm, addr, offset, index)
-			asm = "  mov %s ptr [%s], 0x%X"
-			asm += e.insertGarbageInst() + "\n"
-			_, _ = fmt.Fprintf(builder, asm, op, addr, im)
-		}
-		rem -= opSize
-		index += opSize
-	}
-	builder.WriteString("  ret" + e.insertGarbageInst() + "\n")
-	return builder.String()
-}
-
-func (e *Encoder) selectOpSize64(rem int) int {
-	var size []int
-	switch {
-	case rem < 2:
-		size = []int{1}
-	case rem < 4:
-		size = []int{1, 2}
-	case rem < 8:
-		size = []int{1, 2, 4}
-	default:
-		size = []int{1, 2, 4, 8}
-	}
-	return size[e.rand.Intn(len(size))]
-}
 
 func (e *Encoder) decoderStub() []byte {
 	var decoder []byte
@@ -205,9 +67,15 @@ func (e *Encoder) miniXOR64(inst []byte, key uint64) []byte {
 		numPad = 8 - numPad
 	}
 	inst = append(inst, e.randBytes(numPad)...)
+	seed := e.rand.Uint64()
 	for i := 0; i < len(inst); i += 8 {
-		val := binary.LittleEndian.Uint64(inst[i:i+8]) ^ key
+		val := binary.LittleEndian.Uint64(inst[i : i+8])
+		val ^= key
+		val = ror64(val, uint8(key%64))
+		val ^= seed
+		val = rol64(val, uint8(seed%64))
 		binary.LittleEndian.PutUint64(inst[i:i+8], val)
+		seed = xorShift64(seed)
 	}
 	return inst
 }
@@ -272,6 +140,14 @@ func xorShift64(seed uint64) uint64 {
 	seed ^= seed >> 7
 	seed ^= seed << 17
 	return seed
+}
+
+func ror64(value uint64, bits uint8) uint64 {
+	return value>>bits | value<<(64-bits)
+}
+
+func rol64(value uint64, bits uint8) uint64 {
+	return value<<bits | value>>(64-bits)
 }
 
 func ror(value byte, bits uint8) byte {
