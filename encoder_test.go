3be356fdf5d74d92f4c06ed6ac5964fd861fd837
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-28 21:49:44 +0800

    update TestEncoder and TestEncoderFuzz about use testFindSignature for MinifyMode.

--- encoder_test.go
+++ encoder_test.go
@@ -35,9 +35,12 @@ func TestEncoder(t *testing.T) {
 		}
 		shellcode, err = encoder.Encode(shellcode, 32, opts)
 		require.NoError(t, err)
-		testFindSignature(t, shellcode)
 
 		spew.Dump(shellcode)
+		num := bytes.Count(shellcode, []byte{0x00, 0x00, 0x00})
+		require.Less(t, num, 2)
+		num = bytes.Count(shellcode, []byte{0xFF, 0xFF, 0xFF})
+		require.Less(t, num, 1)
 
 		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
 			return
@@ -51,7 +54,12 @@ func TestEncoder(t *testing.T) {
 		// check shellcode is erased
 		sc := unsafe.Slice((*byte)(unsafe.Pointer(addr)), len(shellcode))
 		require.False(t, bytes.Contains(sc, shellcode))
-		testFindSignature(t, sc)
+
+		spew.Dump(shellcode)
+		num = bytes.Count(shellcode, []byte{0x00, 0x00, 0x00})
+		require.Less(t, num, 2)
+		num = bytes.Count(shellcode, []byte{0xFF, 0xFF, 0xFF})
+		require.Less(t, num, 1)
 	})
 
 	t.Run("x64", func(t *testing.T) {
@@ -73,8 +81,8 @@ func TestEncoder(t *testing.T) {
 		}
 		shellcode, err = encoder.Encode(shellcode, 64, opts)
 		require.NoError(t, err)
-		testFindSignature(t, shellcode)
 
+		testFindSignature(t, shellcode)
 		spew.Dump(shellcode)
 
 		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
@@ -119,9 +127,12 @@ func TestMinifyMode(t *testing.T) {
 		}
 		shellcode, err = encoder.Encode(shellcode, 32, opts)
 		require.NoError(t, err)
-		testFindSignature(t, shellcode)
 
 		spew.Dump(shellcode)
+		num := bytes.Count(shellcode, []byte{0x00, 0x00, 0x00})
+		require.Less(t, num, 2)
+		num = bytes.Count(shellcode, []byte{0xFF, 0xFF, 0xFF})
+		require.Less(t, num, 1)
 
 		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
 			return
@@ -152,8 +163,8 @@ func TestMinifyMode(t *testing.T) {
 		}
 		shellcode, err = encoder.Encode(shellcode, 64, opts)
 		require.NoError(t, err)
-		testFindSignature(t, shellcode)
 
+		testFindSignature(t, shellcode)
 		spew.Dump(shellcode)
 
 		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
@@ -201,7 +212,6 @@ func TestEncoderFuzz(t *testing.T) {
 			addr := loadShellcode(t, output)
 			var val int
 			_, _, _ = syscall.SyscallN(addr, (uintptr)(unsafe.Pointer(&val)))
-			// require.Equal(t, int(addr), int(ret))
 			require.Equal(t, 0x86, val)
 
 			// check shellcode is erased
