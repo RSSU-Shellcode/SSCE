db9c10c415fcf4b9c81e75e05ac0e5c5fa75676a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-29 21:07:07 +0800

    support hex format shellcode with input and output shellcode.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
@@ -9,14 +10,15 @@ import (
 )
 
 var (
-	path string
 	arch int
 	opts ssce.Options
+	ih   bool
+	oh   bool
+	in   string
 	out  string
 )
 
 func init() {
-	flag.StringVar(&path, "i", "", "input shellcode file path")
 	flag.IntVar(&arch, "arch", 64, "set architecture")
 	flag.IntVar(&opts.NumIterator, "iter", 0, "set the number of the iterator")
 	flag.IntVar(&opts.NumTailInst, "tail", 0, "set the number of the garbage inst at tail")
@@ -25,26 +27,18 @@ func init() {
 	flag.BoolVar(&opts.EraseInst, "erase", false, "erase shellcode after call it")
 	flag.BoolVar(&opts.NoIterator, "no-iter", false, "no iterator, not recommend")
 	flag.BoolVar(&opts.NoGarbage, "no-garbage", false, "no garbage, not recommend")
+	flag.BoolVar(&ih, "ih", false, "input shellcode with hex format")
+	flag.BoolVar(&oh, "oh", false, "output shellcode with hex format")
+	flag.StringVar(&in, "i", "", "set input shellcode file path")
 	flag.StringVar(&out, "o", "", "set output shellcode file path")
 	flag.Parse()
 }
 
 func main() {
-	if path == "" {
+	if in == "" {
 		flag.Usage()
 		return
 	}
-
-	fmt.Printf("read input shellcode from \"%s\"\n", path)
-	shellcode, err := os.ReadFile(path)
-	checkError(err)
-	fmt.Println("raw shellcode size:", len(shellcode))
-
-	encoder := ssce.NewEncoder()
-	shellcode, err = encoder.Encode(shellcode, arch, &opts)
-	checkError(err)
-	fmt.Println("encoded shellcode size:", len(shellcode))
-
 	if out == "" {
 		switch arch {
 		case 32:
@@ -53,6 +47,25 @@ func main() {
 			out = "output_x64.bin"
 		}
 	}
+
+	fmt.Printf("read input shellcode from \"%s\"\n", in)
+	shellcode, err := os.ReadFile(in)
+	checkError(err)
+	if ih {
+		data, err := hex.DecodeString(string(shellcode))
+		checkError(err)
+		shellcode = data
+	}
+	fmt.Println("raw shellcode size:", len(shellcode))
+
+	encoder := ssce.NewEncoder()
+	shellcode, err = encoder.Encode(shellcode, arch, &opts)
+	checkError(err)
+	fmt.Println("encoded shellcode size:", len(shellcode))
+	if oh {
+		shellcode = []byte(hex.EncodeToString(shellcode))
+	}
+
 	fmt.Printf("write output shellcode to \"%s\"\n", out)
 	err = os.WriteFile(out, shellcode, 0600)
 	checkError(err)
