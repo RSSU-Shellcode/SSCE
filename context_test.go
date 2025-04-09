051316b130f76b35009ffc1eb5a938cbaf544fb0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-09 13:36:34 +0800

    add TestSaveContext and TestRestoreContext for check instructions.

--- context_test.go
+++ context_test.go
@@ -1,1 +1,47 @@
 package ssce
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func TestSaveContext(t *testing.T) {
+	encoder := NewEncoder(1)
+	encoder.opts = new(Options)
+
+	t.Run("x86", func(t *testing.T) {
+		encoder.arch = 32
+
+		inst := encoder.saveContext()
+		spew.Dump(inst)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		encoder.arch = 64
+
+		inst := encoder.saveContext()
+		spew.Dump(inst)
+	})
+}
+
+func TestRestoreContext(t *testing.T) {
+	encoder := NewEncoder(1)
+	encoder.opts = new(Options)
+
+	t.Run("x86", func(t *testing.T) {
+		encoder.arch = 32
+		encoder.saveContext()
+
+		inst := encoder.restoreContext()
+		spew.Dump(inst)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		encoder.arch = 64
+		encoder.saveContext()
+
+		inst := encoder.restoreContext()
+		spew.Dump(inst)
+	})
+}
