b94197a894ffd1efdf12911e9efbea4082ecceab
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-09 13:25:58 +0800

    add TestToDB and TestToHex for check common and empty data.

--- assembly_test.go
+++ assembly_test.go
@@ -1,1 +1,27 @@
 package ssce
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestToDB(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		data := []byte{1, 2, 3, 4}
+		output := toDB(data)
+
+		expected := ".byte 0x01, 0x02, 0x03, 0x04, "
+		require.Equal(t, expected, output)
+	})
+
+	t.Run("empty bytes", func(t *testing.T) {
+		output := toDB(nil)
+		require.Empty(t, output)
+	})
+}
+
+func TestToHex(t *testing.T) {
+	output := toHex(15)
+	require.Equal(t, "0xF", output)
+}
