fd98bfb81fa8428e5fbccefd7ef81555f0674abb
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-08 20:10:53 +0800

    add TestGarbageJumpShort and TestGarbageDestroyFlag, wait generator.

--- garbage_test.go
+++ garbage_test.go
@@ -1,1 +1,26 @@
 package ssce
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGarbageJumpShort(t *testing.T) {
+	encoder := NewEncoder(1)
+	encoder.opts = new(Options)
+
+	for i := 0; i < 1000; i++ {
+		size := len(encoder.garbageJumpShort(2, 5))
+		require.True(t, size >= 2+2 && size <= 5+2)
+	}
+}
+
+func TestGarbageDestroyFlag(t *testing.T) {
+	encoder := NewEncoder(1)
+	encoder.opts = new(Options)
+
+	for i := 0; i < 1000; i++ {
+		encoder.garbageDestroyFlag()
+	}
+}
