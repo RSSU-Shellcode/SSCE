3ad54a5862a872d0b4c60fc52e40ec19ab920e22
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-08 20:09:04 +0800

    implement garbageMultiByteNOP, improve garbageInst and garbageInstShort.

--- garbage.go
+++ garbage.go
@@ -1,38 +1,41 @@
 package ssce
 
+// the output garbage instruction length is no limit.
 func (e *Encoder) garbageInst() []byte {
 	if e.opts.NoGarbage {
 		return nil
 	}
-	switch e.rand.Intn(2) {
+	switch e.rand.Intn(3) {
 	case 0:
 		return nil
 	case 1:
 		return e.garbageJumpShort(2, 16)
+	case 2:
+		return e.garbageMultiByteNOP()
 	default:
 		panic("invalid garbage instruction selection")
 	}
 }
 
+// the output garbage instruction length is <= 7 bytes.
 func (e *Encoder) garbageInstShort() []byte {
 	if e.opts.NoGarbage {
 		return nil
 	}
-	switch e.rand.Intn(2) {
+	switch e.rand.Intn(3) {
 	case 0:
 		return nil
 	case 1:
 		return e.garbageJumpShort(2, 5)
+	case 2:
+		return e.garbageMultiByteNOP()
 	default:
 		panic("invalid garbage instruction selection")
 	}
 }
 
-// jmp short [4-128)
+// 0xEB, rel, [min, max] random bytes.
 func (e *Encoder) garbageJumpShort(min, max int) []byte {
-	if e.opts.NoGarbage {
-		return nil
-	}
 	if min < 1 || max > 127 {
 		panic("garbage jump short length out of range")
 	}
@@ -43,16 +46,29 @@ func (e *Encoder) garbageJumpShort(min, max int) []byte {
 	return jmp
 }
 
-func (e *Encoder) randBytes(n int) []byte {
-	buf := make([]byte, n)
-	_, _ = e.rand.Read(buf)
-	return buf
+func (e *Encoder) garbageMultiByteNOP() []byte {
+	var nop []byte
+	switch e.rand.Intn(6) {
+	case 0:
+		nop = []byte{0x90}
+	case 1:
+		nop = []byte{0x66, 0x90}
+	case 2:
+		nop = []byte{0x0F, 0x1F, 0x00}
+	case 3:
+		nop = []byte{0x0F, 0x1F, 0x40, 0x00}
+	case 4:
+		nop = []byte{0x0F, 0x1F, 0x44, 0x00, 0x00}
+	case 5:
+		nop = []byte{0x66, 0x0F, 0x1F, 0x44, 0x00, 0x00}
+	}
+	return nop
 }
 
-func (e *Encoder) randString(n int) []byte {
-	buf := make([]byte, n)
-	for i := 0; i < n; i++ {
-		buf[i] = byte(32 + e.rand.Intn(95)) // [32, 126]
-	}
-	return buf
+// random add, sub, inc, dec
+func (e *Encoder) garbageDestroyFlag() []byte {
+	inst := []byte{0x9C} // pushfd/pushfq
+
+	inst = append(inst, 0x9D) // popfd/popfq
+	return inst
 }
