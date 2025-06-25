b3042364cd33dad3c87ec7e86b24c13b064a71ce
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-25 12:56:14 +0800

    remove TestGarbageDestroyFlag, add TestGarbageTemplate, update TestGarbageJumpShort.

--- garbage_test.go
+++ garbage_test.go
@@ -14,13 +14,20 @@ func TestGarbageJumpShort(t *testing.T) {
 		size := len(encoder.garbageJumpShort(2, 5))
 		require.True(t, size >= 2+2 && size <= 5+2)
 	}
+
+	err := encoder.Close()
+	require.NoError(t, err)
 }
 
-func TestGarbageDestroyFlag(t *testing.T) {
+func TestGarbageTemplate(t *testing.T) {
 	encoder := NewEncoder(1)
 	encoder.opts = new(Options)
 
 	for i := 0; i < 1000; i++ {
-		encoder.garbageDestroyFlag()
+		data := encoder.garbageTemplate()
+		require.NotNil(t, data)
 	}
+
+	err := encoder.Close()
+	require.NoError(t, err)
 }
