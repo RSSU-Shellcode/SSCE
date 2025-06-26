77cd9cd232203608f22f06dbbc2443419a35707e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-26 12:15:07 +0800

    update TestSaveContext and TestRestoreContext about NewEncoder.

--- context_test.go
+++ context_test.go
@@ -4,44 +4,69 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSaveContext(t *testing.T) {
-	encoder := NewEncoder(1)
-	encoder.opts = new(Options)
-
 	t.Run("x86", func(t *testing.T) {
+		encoder := NewEncoder(0)
 		encoder.arch = 32
+		encoder.opts = new(Options)
+		err := encoder.initAssembler()
+		require.NoError(t, err)
 
 		inst := encoder.saveContext()
 		spew.Dump(inst)
+
+		err = encoder.Close()
+		require.NoError(t, err)
 	})
 
 	t.Run("x64", func(t *testing.T) {
+		encoder := NewEncoder(0)
 		encoder.arch = 64
+		encoder.opts = new(Options)
+		err := encoder.initAssembler()
+		require.NoError(t, err)
 
 		inst := encoder.saveContext()
 		spew.Dump(inst)
+
+		err = encoder.Close()
+		require.NoError(t, err)
 	})
 }
 
 func TestRestoreContext(t *testing.T) {
-	encoder := NewEncoder(1)
-	encoder.opts = new(Options)
-
 	t.Run("x86", func(t *testing.T) {
+		encoder := NewEncoder(0)
 		encoder.arch = 32
+		encoder.opts = new(Options)
+		err := encoder.initAssembler()
+		require.NoError(t, err)
+
 		encoder.saveContext()
 
 		inst := encoder.restoreContext()
 		spew.Dump(inst)
+
+		err = encoder.Close()
+		require.NoError(t, err)
 	})
 
 	t.Run("x64", func(t *testing.T) {
+		encoder := NewEncoder(0)
 		encoder.arch = 64
+		encoder.opts = new(Options)
+		err := encoder.initAssembler()
+		require.NoError(t, err)
+
 		encoder.saveContext()
 
 		inst := encoder.restoreContext()
 		spew.Dump(inst)
+
+		err = encoder.Close()
+		require.NoError(t, err)
 	})
 }
