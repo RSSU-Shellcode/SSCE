e5a2e2e5204a2022f3fc45761d1d9c5fe8a5a730
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-06-28 14:26:45 +0800

    hybrid_x64.asm: implement hybrid garbage instruction template.

--- junk/hybrid_x64.asm
+++ junk/hybrid_x64.asm
@@ -1,5 +1,28 @@
 .code64
 
 push {{.Reg.rax}}
+
+{{if .Switch.A}}
 mov {{.Reg.rax}}, {{.Reg.rbx}}
+{{end}}
+
+{{if .Switch.B}}
+xor {{.Reg.rax}}, {{.Reg.rcx}}
+{{end}}
+
+{{if .Switch.C}}
+xor {{.Reg.rax}}, {{.DWORD.D}}
+{{end}}
+
+{{if .Switch.D}}
+mov {{.Reg.rax}}, {{.WORD.E}}
+{{end}}
+
+{{if .Switch.E}}
+ror {{.Reg.rax}}, {{.Less32.A}}
+xor {{.Reg.rax}}, {{.Reg.rdi}}
+rol {{.Reg.rax}}, {{.Less32.B}}
+{{end}}
+
 pop {{.Reg.rax}}
+
