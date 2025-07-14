658d9d47d6fc7643d84936eb0368a06e5d5e43b3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-14 16:46:02 +0800

    add register ebp to registerX86 and rbp to registerX64.

--- encoder.go
+++ encoder.go
@@ -17,13 +17,13 @@ import (
 
 var (
 	registerX86 = []string{
-		"eax", "ebx", "ecx",
-		"edx", "esi", "edi",
+		"eax", "ebx", "ecx", "edx",
+		"ebp", "esi", "edi",
 	}
 
 	registerX64 = []string{
-		"rax", "rbx", "rcx",
-		"rdx", "rsi", "rdi",
+		"rax", "rbx", "rcx", "rdx",
+		"rbp", "rsi", "rdi",
 		"r8", "r9", "r10", "r11",
 		"r12", "r13", "r14", "r15",
 	}
