c47797394d4bd2364305dec25d8bfd11a38b08bd
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-08 15:04:50 +0800

    add TestInspectMiniDecoderTemplate and TestInspectLoaderTemplate.

--- inspect_test.go
+++ inspect_test.go
@@ -8,6 +8,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestInspectMiniDecoderTemplate(t *testing.T) {
+	t.Run("x86", func(t *testing.T) {
+		asm, inst, err := InspectMiniDecoderTemplate(32, defaultMiniDecoderX86)
+		require.NoError(t, err)
+		fmt.Println(asm)
+		spew.Dump(inst)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		asm, inst, err := InspectMiniDecoderTemplate(64, defaultMiniDecoderX64)
+		require.NoError(t, err)
+		fmt.Println(asm)
+		spew.Dump(inst)
+	})
+}
+
+func TestInspectLoaderTemplate(t *testing.T) {
+	t.Run("x86", func(t *testing.T) {
+		asm, inst, err := InspectLoaderTemplate(32, defaultLoaderX86)
+		require.NoError(t, err)
+		fmt.Println(asm)
+		spew.Dump(inst)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		asm, inst, err := InspectLoaderTemplate(64, defaultLoaderX64)
+		require.NoError(t, err)
+		fmt.Println(asm)
+		spew.Dump(inst)
+	})
+}
+
 func TestInspectJunkCodeTemplate(t *testing.T) {
 	t.Run("x86", func(t *testing.T) {
 		for _, src := range defaultJunkCodeX86 {
